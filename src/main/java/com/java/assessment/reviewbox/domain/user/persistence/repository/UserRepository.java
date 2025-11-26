555cae667c7feb355e9ec77cf137b038533bba9b
sehyuk080101 <qzjaiqa0101@gmail.com>
2025-11-26 23:10:35 +0900

    유저 리파지토리 구현

---
+++ src/main/java/com/java/assessment/reviewbox/domain/user/persistence/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.java.assessment.reviewbox.domain.user.persistence.repository;
+
+import com.java.assessment.reviewbox.domain.user.persistence.entity.UserEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
