512901e9ba007d1d69ce4b122f4a927e21fd2c4f
sehyuk080101 <qzjaiqa0101@gmail.com>
2025-11-26 23:21:40 +0900

    jpa config 추가

---
+++ src/main/java/com/java/assessment/reviewbox/global/jpa/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.java.assessment.reviewbox.global.jpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
